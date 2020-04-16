9a0c4ee6d68f05b29f105dcbd5ebe0d663d6823a
Abdullah Waheed <iwadday@gmail.com>
2020-04-16 20:05:38 +0500

    Passport Token Testing

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
 Route::get('/', function () {
     return view('index');
 });
@@ -31,4 +34,56 @@ Route::group(['middleware' => 'auth'], function() {
         Route::get('/home', 'HomeController@index')->name('home');
 
     });
+});
+
+
+Route::get('/redirect', function (Request $request) {
+    $request->session()->put('state', $state = Str::random(40));
+
+    $request->session()->put('code_verifier', $code_verifier = Str::random(128));
+
+    $codeChallenge = strtr(rtrim(
+        base64_encode(hash('sha256', $code_verifier, true))
+        , '='), '+/', '-_');
+
+    $query = http_build_query([
+        'client_id' => '4',
+        'redirect_uri' => 'https://fvm.veem.online/callback',
+        'response_type' => 'code',
+        'scope' => '',
+        'state' => $state,
+        'code_challenge' => $codeChallenge,
+        'code_challenge_method' => 'S256',
+    ]);
+
+    return redirect('https://fvm.veem.online/oauth/authorize?'.$query);
+});
+
+Route::get('/callback', function (Request $request) {
+    $state = $request->session()->pull('state');
+
+    $codeVerifier = $request->session()->pull('code_verifier');
+
+    throw_unless(
+        strlen($state) > 0 && $state === $request->state,
+        InvalidArgumentException::class
+    );
+
+    $http = new GuzzleHttp\Client();
+    $response = $http->post('https://fvm.veem.online/oauth/token', [
+        'form_params' => [
+            'grant_type' => 'authorization_code',
+            'client_id' => '1',
+            'client_secret' => 'lbk9leQq8cgUafDzFNvyyWQMfeSb742uK4IZfosq',
+            'redirect_uri' => 'https://fvm.veem.online/login_success',
+            'code_verifier' => $codeVerifier,
+            'code' => $request->code,
+        ],
+    ]);
+
+    return json_decode((string) $response->getBody(), true);
+});
+
+Route::get('/login_success', function(Request $request) {
+    return response()->json($request->all());
 });
